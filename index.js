36f205a4ed01760ec5f9cd5a0b494f53be681c7a
groot737 <giorgiquchuloria7@gmail.com>
2023-10-30 00:03:38 +0400

    added mongodb

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j4b7hrp.mongodb.net/?retryWrites=true&w=majority`)
+ .then(() => {
+    console.log('connected')
+ })
+
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
